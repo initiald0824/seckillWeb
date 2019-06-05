9691bf760eedaa112f6fdc61d85969e184fdd3c1
initiald0824 <initiald0824@163.com>
2019-06-05 21:15:44 +0800

    *

--- src/components/goods/goodsDetail.js
+++ src/components/goods/goodsDetail.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+import pathToRegexp from 'path-to-regexp';
 
 class GoodsDetail extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      goodsId: null
+    }
+  }
+
+  componentWillMount () {
+    this.init()
+  }
+
+  init () {
+    this.initGoodsId();
+  }
+
+  initGoodsId () {
+    const re = pathToRegexp('/goodsDetail/:id');
+    const match = re.exec(this.props.location.pathname);
+    if (match) {
+      this.setState({
+        goodsId: match[1]
+      });
+    }
+  }
+
   render () {
     return (
       <div>GoodsDetail</div>
@@ -8,4 +36,4 @@ class GoodsDetail extends Component {
   }
 }
 
-export default GoodsDetail;
+export default withRouter(GoodsDetail);
